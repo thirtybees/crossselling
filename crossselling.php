e2eafe27d6c23c2a2239b1b025144a4e323a2e77
Markus Hitter <mah@jump-ing.de>
2018-06-05 23:00:11 +0200

    Add missing, mandatory main class constructor entries.

--- crossselling.php
+++ crossselling.php
@@ -44,6 +44,7 @@ class CrossSelling extends Module
 
         $this->displayName = $this->l('Block Cross Selling');
         $this->description = $this->l('Adds a "Customers who bought this product also bought..." section to every product page.');
+        $this->tb_versions_compliancy = '> 1.0.0';
         $this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => '1.6.99.99');
     }
 
